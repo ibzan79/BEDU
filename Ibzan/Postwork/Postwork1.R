50c2d52c236d3c6f13a7a1862706c58a0186c55f
ibzan79 <45348626+ibzan79@users.noreply.github.com>
2021-06-22 17:48:51 -0500

    Add files via upload

--- Ibzan/Postwork/Postwork1.R
+++ Ibzan/Postwork/Postwork1.R
@@ -4,9 +4,11 @@ setwd("c:/Users/ibzan/OneDrive/Documentos/Cursos/BEDU/Modulo2/1 - Introduccion a
 
 liga_19.20.df <- read.csv(url_liga)
 
-head(liga_19.20.df, 1)
+head(liga_19.20.df)
 
 install.packages("dplyr")
 library(dplyr)
 
 df_fthg <- select(liga_19.20.df, "FTHG", "FTAG")
+
+?table
